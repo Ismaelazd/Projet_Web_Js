7ab67a3399099527578164b37fd49f2ff3f8db84
Ismael <ismaelazd@hotmail.com>
2019-12-17 10:29:28 +0100

    Debut de la partie JS

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+
+let darkMode = document.querySelectorAll('.blackMode');
+
+
+(function blackMode(){
+
+    darkMode.forEach(element => {
+        console.log(element);
+        
+    });
+
+})()
+
